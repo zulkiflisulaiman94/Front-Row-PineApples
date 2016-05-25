6fedf47f106f3d4c29acac1111dab0c9fc94681a
moniimon <100044163@student.swin.edu.au>
2016-05-25 17:36:23 +1000

    in commit

---
+++ src/Latte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SwinGameSDK;
+
+namespace MyGame
+{
+	public class Latte : Coffee
+	{
+		public Latte (int t) : base(t,  350,  350)
+		{
+			cImg = SwinGame.LoadBitmapNamed("latte", "latte.png");
+			Console.WriteLine("bitmap return");
+		}
+
+
+
+		public override void Draw()
+		{
+			Point2D ptt = default (Point2D);
+			ptt = SwinGame.PointAt (posX, posY);
+			SwinGame.DrawBitmapOnScreen(cImg, ptt);	
+		}
+		public override bool IsAt(Point2D pt)
+		{
+			return SwinGame.PointInRect (pt ,posX, posY, (posX + 20), (posY + 20));
+		}
+	}
+}
+
