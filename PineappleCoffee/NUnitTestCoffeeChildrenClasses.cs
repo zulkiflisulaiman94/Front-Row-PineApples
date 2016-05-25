94b14e3cce33927298553ea8bc84026579cc000f
moniimon <100044163@student.swin.edu.au>
2016-05-25 17:15:10 +1000

    moniis testing

---
+++ PineappleCoffee/NUnitTestCoffeeChildrenClasses.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MyGame
+{
+	[TestFixture ()]
+	public class NUnitTestCoffeeChildrenClasses
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
