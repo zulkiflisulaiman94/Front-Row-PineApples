b99ca84a8c35dfbc9b3174065ad70972c69c82ee
moniimon <100044163@student.swin.edu.au>
2016-05-26 14:00:40 +1000

    Adding moniis test to the rep

---
+++ src/MoniisTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using SwinGameSDK;
+
+namespace MyGame
+{
+	[TestFixture ()]
+	public class MoniisTest
+	{
+		[Test ()]
+		public void TestLatteDraws ()
+		{
+			// ARRANGE //
+			Point2D p 	= new Point2D();
+			Latte l 	= new Latte(30);
+			// ACT //
+			l.Draw();
+			p = SwinGame.PointAt(351, 351);
+			// ASSERT //
+			Assert.IsTrue(l.IsAt(p));
+		}
+	}
+}
+
