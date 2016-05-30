51bb7feffa82f3c45e3c1c6dc277dc076d4ddbf3
s5353688 <5353688@student.swin.edu.au>
2016-05-30 15:00:20 +1000

    extend

--- PineappleCoffee/src/Prizes.cs
+++ PineappleCoffee/src/Prizes.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using SwinGameSDK;
 
 namespace MyGame
 {
 	public class Prizes
 	{
-		public Prizes ()
+
+		private int _score;
+
+
+		public Prizes (int s)
 		{
+			_score = s;
+		}
+
+		public void returnPrize()
+		{
+			if ((int)_score < 500000)
+			{
+				Text.DrawText ("Better luck next time!", Color.Black, 300, 250);
+			}
+			else if ((int)_score < 1000000)
+			{
+				Text.DrawText ("Buy one regular Coffee", Color.Black, 300, 250);
+				Text.DrawText ("Get one free", Color.Black, 320, 300);
+			}
+			else if ((int)_score < 1500000)
+			{
+				Text.DrawText ("One free regular Coffee", Color.Black, 300, 250);
+			}
 		}
 	}
 }
