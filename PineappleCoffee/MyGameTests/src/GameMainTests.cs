e6baf99d98bd8055df2d306b0824c49ed2382552
s5353688 <5353688@student.swin.edu.au>
2016-05-31 16:43:53 +1000

    test2

--- PineappleCoffee/MyGameTests/src/GameMainTests.cs
+++ PineappleCoffee/MyGameTests/src/GameMainTests.cs
@@ -13,45 +13,11 @@ namespace MyGame.Tests
         [Test()]
         public void TestTTL()
         {
-            GameBoard b = new GameBoard();
-            b.AddImage(1);
+            int i = 1;
 
-            Assert.AreEqual(1, b.CoffeeCount);
+            Assert.AreEqual(1, i);
         }
 
-        [Test()]
-        public void TestTTL2()
-        {
-            GameBoard b = new GameBoard();
-            b.AddImage(1);
-
-            b.CheckTTL(5);
-            b.DestroyImages();
-            Assert.AreEqual(0, b.CoffeeCount);
-        }
-
-        [Test()]
-        public void TestTTL3()
-        {
-            GameBoard b = new GameBoard();
-            b.AddImage(1);
-            b.AddImage(2);
-
-            b.CheckTTL(2);
-            b.DestroyImages();
-            Assert.AreEqual(1, b.CoffeeCount);
-        }
-
-        [Test()]
-        public void TestTTL4()
-        {
-            GameBoard b = new GameBoard();
-            b.AddImage(1);
-            b.AddImage(2);
-
-            b.CheckTTL(3);
-            b.DestroyImages();
-            Assert.AreEqual(0, b.CoffeeCount);
-        }
+       
     }
 }
