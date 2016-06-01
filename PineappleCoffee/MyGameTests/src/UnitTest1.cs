d05b62545b4767098aefe868af764f98f71dbacf
s5353688 <5353688@student.swin.edu.au>
2016-06-01 17:53:14 +1000

    newcontent

---
+++ PineappleCoffee/MyGameTests/src/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using MyGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MyGame.Tests
+{
+    [TestFixture()]
+    public class ChrisTest2
+    {
+        [Test()]
+        public void TestScoreCtrl()
+        {
+            ScoreControler s = new ScoreControler();
+            int i = s.FetchScore();
+
+            Assert.AreEqual(1, i);
+        }
+        [Test()]
+        public void TestScoreCtrl2()
+        {
+            ScoreControler s = new ScoreControler();
+
+            s.add(2);
+
+            int i = s.FetchScore();
+
+            Assert.AreEqual(3, i);
+        }
+        [Test()]
+        public void TestScoreCtrl3()
+        {
+            ScoreControler s = new ScoreControler();
+
+            s.add(2);
+
+            int i = s.FetchScore();
+
+            Assert.AreEqual(3, i);
+        }
+        [Test()]
+        public void TestScoreCtrl4()
+        {
+            ScoreControler s = new ScoreControler();
+
+            s.add(2);
+            s.add(-1);
+
+            int i = s.FetchScore();
+
+            Assert.AreEqual(2, i);
+        }
+      
+
+
+
+
+    }
+}
---
+++ PineappleCoffee/src/ScoreControler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGame
+{
+    public class ScoreControler
+    {
+        private int _score;
+   
+
+    public ScoreControler()
+    {
+        _score = 1;
+    }
+
+        public int FetchScore()
+        {
+            return _score;
+        }
+
+        public void add(int i)
+        {
+            _score = _score + i;
+        }
+    }
+}
