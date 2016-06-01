7c1370e0431278b9c9352a1d6ce5c19c5efb10e4
zulkiflisulaiman94 <101018091@student.swin.edu.au>
2016-06-01 17:57:03 +1000

    sdf123we4

--- PineappleCoffee/src/Latte.cs
+++ PineappleCoffee/src/Latte.cs
@@ -6,6 +6,7 @@ namespace MyGame
 	public class Latte : Coffee
 	{
         private Bitmap _Cimg; 
+
 		public Latte (int t) : base(t,  350,  350)
 		{
 			_Cimg = SwinGame.LoadBitmapNamed("latte", "latte.png");
@@ -17,6 +18,7 @@ namespace MyGame
 			p = SwinGame.PointAt (posX, posY);
 			SwinGame.DrawBitmapOnScreen(_Cimg, p);	
 		}
+
 		public override bool IsAt(Point2D pt)
 		{
 			return SwinGame.PointInRect (pt ,posX, posY, (posX + 20), (posY + 20));
